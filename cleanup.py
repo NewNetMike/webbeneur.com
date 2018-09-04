45669eb04b86a34a1079fa6691d42c7e0a2a02ae
2Pac <itsyourpalmike@gmail.com>
2018-09-03 18:41:29 -0700

    ...

---
+++ cleanup.py
@@ -0,0 +1,12 @@
+import os, fnmatch
+def findReplace(directory, find, replace, filePattern):
+    for path, dirs, files in os.walk(os.path.abspath(directory)):
+        for filename in fnmatch.filter(files, filePattern):
+            filepath = os.path.join(path, filename)
+            with open(filepath, encoding="utf8") as f:
+                s = f.read()
+            s = s.replace(find, replace)
+            with open(filepath, "w", encoding="utf8") as f:
+                f.write(s)
+                
+findReplace(".", "http://", "https://", "*.html")
